4ab4c3df309e9a3c128ca68945d5280f951a9633
KGerretsen <33416856+KGerretsen@users.noreply.github.com>
2018-01-15 13:43:38 +0100

    TableClickEvent gekoppeld
    Juiste nummers bij elkaar gekoppeld voor wijzigen van account

--- Netflix  app/Netflixstatistix/src/gui/TableClickEvent.java
+++ Netflix  app/Netflixstatistix/src/gui/TableClickEvent.java
@@ -22,9 +22,9 @@ public class TableClickEvent implements MouseListener {
     private JComboBox comboBoxselect;
     private JComboBox comboBox_1;
 
-    public TableClickEvent(JTable talbe,JComboBox comboBoxselect,JTextField textField, JTextField textField_1, JTextField textField_2, JTextField textField_3, JTextField textField_4, JTextField textField_5, JTextField textField_6, JTextField textField_7, JTextField textField_8, JPasswordField passwordField ,JComboBox comboBox1, JComboBox comboBox2,JComboBox comboBox3,JComboBox comboBox4)
+    public TableClickEvent(JTable table,JComboBox comboBoxselect,JTextField textField, JTextField textField_1, JTextField textField_2, JTextField textField_3, JTextField textField_4, JTextField textField_5, JTextField textField_6, JTextField textField_7, JTextField textField_8, JPasswordField passwordField ,JComboBox comboBox1, JComboBox comboBox2,JComboBox comboBox3,JComboBox comboBox4)
     {
-        this.table = talbe;
+        this.table = table;
         this.comboBoxselect = comboBoxselect;
         this.textField = textField_5;
         this.passwordField = passwordField;
@@ -35,7 +35,8 @@ public class TableClickEvent implements MouseListener {
         this.textField_5 = textField_3;
         this.textField_6 = textField_2;
         this.textField_7 = textField_1;
-        
+        this.textField_8 = textField;
+
     }
     @Override
     public void mouseClicked(MouseEvent e) {
@@ -43,21 +44,23 @@ public class TableClickEvent implements MouseListener {
         {
             int row = table.getSelectedRow();
             TableModel model = table.getModel();
-            textField.setText(table.getModel().getValueAt(row,2).toString());
-            textField_1.setText(table.getModel().getValueAt(row,3).toString());
-            textField_2.setText(table.getModel().getValueAt(row,4).toString());
-            textField_3.setText(table.getModel().getValueAt(row,5).toString());
-            textField_4.setText(table.getModel().getValueAt(row,6).toString());
+            textField.setText(table.getModel().getValueAt(row,3).toString());
+            textField_1.setText(table.getModel().getValueAt(row,4).toString());
+            textField_2.setText(table.getModel().getValueAt(row,5).toString());
+            textField_3.setText(table.getModel().getValueAt(row,2).toString());
+            textField_4.setText(table.getModel().getValueAt(row,1).toString());
             textField_5.setText(table.getModel().getValueAt(row,6).toString());
             textField_6.setText(table.getModel().getValueAt(row,7).toString());
             textField_7.setText(table.getModel().getValueAt(row,8).toString());
+            textField_8.setText(table.getModel().getValueAt(row,9).toString());
         }
-        else if (comboBoxselect.getSelectedItem() == "Profiel")
-        {
-
+        else if (comboBoxselect.getSelectedItem() == "Profiel"){
+//            int row = table.getSelectedRow();
+//            TableModel model = table.getModel();
+//            textField_3.setText(table.getModel().getValueAt(row,2).toString());
+//            textField.setText(table.getModel().getValueAt(row,3).toString());
         }
-        else if (comboBoxselect.getSelectedItem() == "Programma")
-        {
+        else if (comboBoxselect.getSelectedItem() == "Programma"){
 
         }
     }
